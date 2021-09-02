55581a916c0b419e789908bc78bf03bb2b692d23
Dragos <dyenigma@gmail.com>
2021-09-02 15:43:11 +0300

    merged with master

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,11 @@ public class Main {
         System.out.println("Our team");
         System.out.println("11 members");
         printDiana();
+        printDragos();
+    }
+
+    public static void printDragos() {
+        System.out.println("Petcu Dragos");
     }
 
     public static void printDiana() {
