2970f1c3fff1b5c3efd6f7817657e3def53dd32f
Andrei-Alexandru Mariciuc <mariciucandreii@gmail.com>
2021-09-02 16:29:01 +0300

    ourproject-123: naming conventions resolved

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,18 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.println("Our team");
         System.out.println("11 members");
         printDiana();
-        andreiMethod();
+        printAndreiMethod();
     }
 
     public static void printDiana() {
         System.out.println("Diana");
     }
 
-    public static void andreiMethod() {
+    public static void printAndreiMethod() {
         System.out.println("Mariciu Andrei Alexandru");
     }
 
