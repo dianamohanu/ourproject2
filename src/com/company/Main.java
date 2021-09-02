5a81b6b8083abceb2c6e5825e15c1d76056018b6
Andrei-Alexandru Mariciuc <mariciucandreii@gmail.com>
2021-09-02 15:50:56 +0300

    ourproject-123: Added my name method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.println("Our team");
         System.out.println("11 members");
         printDiana();
+        andreiMethod();
     }
 
     public static void printDiana() {
