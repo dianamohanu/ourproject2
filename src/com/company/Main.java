a75609d50432f733e7c33471aa649260c4acf9bc
Gabriela-Gabor <gaborgabriela2001@yahoo.com>
2021-09-02 15:17:23 +0300

    task_gabriela: printMyName

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.println("Our team");
         System.out.println("11 members");
+        printMyName();
     }
 
     public static void login() {
         System.out.println("123");
+    }
+
+    public static void printMyName() {
+        System.out.println("Gabriela");
     }
 }
